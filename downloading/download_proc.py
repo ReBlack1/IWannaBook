cc6a0aefb78143b5e98fba2d5e1f3f52dcb94bcd
Evgenii <ReBlack1@yandex.ru>
2020-11-08 23:26:26 +0300

    скачка проще

--- downloading/download_proc.py
+++ downloading/download_proc.py
@@ -1,16 +1,13 @@
-import downloader
+import downloading.downloader as downloader
 import time
 
-for i in range(134, 600):
-    print("Скачано = ", i*1000)
-
-    for number in range(1000):
-        if number + (1000*i) < 151019:
-            continue
-        while 1:
-            try:
-                downloader.flibusta_load_book(number + (1000*i), r'E:\books')
-                break
-            except Exception as e:
-                print("Ошибка на книге", number + (1000*i), str(e))
-                time.sleep(2)
+for number in range(400000, 500000):
+    if number % 1000 == 0:
+        print("Скачано " + str(number))
+    while 1:
+        try:
+            downloader.flibusta_load_book(number, r'E:\books')
+            break
+        except Exception as e:
+            print("Ошибка на книге", number, str(e))
+            time.sleep(2)
