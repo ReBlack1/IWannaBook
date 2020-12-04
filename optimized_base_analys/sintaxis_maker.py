d68b040925da6f5cea678bb5c6b484050fcc2b47
Evgenii <ReBlack1@yandex.ru>
2020-12-04 19:31:31 +0300

    Оптимизированное вычисление синтаксиса

--- optimized_base_analys/sintaxis_maker.py
+++ optimized_base_analys/sintaxis_maker.py
@@ -2,36 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import book_manager
-from language_tools.udpipe import UDPipe
-from base_analys.base_helper import get_base_info_from_book_xml, get_sintax_list
+from base_analys.base_helper import get_base_info_from_book_xml
 import config
 import time
 from lxml import etree
 import re
 
-
-from ipymarkup import show_dep_ascii_markup as show_markup
+from ipymarkup import show_dep_ascii_markup as show_markup  # Для красивого вывода синтаксиса
 from razdel import sentenize, tokenize
 from navec import Navec
 from slovnet import Syntax
 
-import multiprocessing
-print(multiprocessing.cpu_count())
+import pickle
+import zipfile
 
-"""
-def f(x):
-    while 1:
-        pass  # infinite loop
-
-import multiprocessing as mp
-n_cores = mp.cpu_count()
-with mp.Pool(n_cores) as p:
-    p.map(f, range(n_cores))
-"""
-
-exit()
-
-def get_sintax_list2(syntax, book_xml):
+def get_sintax_list_with_navec(syntax_parser, book_xml):
     _xpath = etree.XPath(r'//*')
     dom = etree.XML(book_xml)
     ans = _xpath(dom)
@@ -46,8 +31,9 @@ def get_sintax_list2(syntax, book_xml):
                 continue
             """ Построчная обработка книги перед предпроцессингом (Только текста) """
             tokens = [_.text for _ in tokenize(i.text)]
-            chunk.append(tokens)
-    syntax_map = syntax.map(chunk)
+            if tokens:
+                chunk.append(tokens)
+    syntax_map = syntax_parser.map(chunk)
 
     for markup in syntax_map:
         # nvert CoNLL-style format to source, target indices
@@ -61,30 +47,31 @@ def get_sintax_list2(syntax, book_xml):
         sintax_list.append((words, deps))
     return sintax_list
 
+
 if __name__ == '__main__':
-    # udpipe = UDPipe()
-    book_num = 40000
-    book_path = config.raw_book_path + r'\flibusta_' + str(book_num) + '.zip'
-    book_xml = book_manager.open_book(book_path)
-    book_info = get_base_info_from_book_xml(book_xml)
-    print(book_num, book_info)
-
-
-    # st = time.time()
-    # sintax_list = get_sintax_list(udpipe, book_xml)
-    # print(time.time() - st)
 
     navec = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
     syntax_parser = Syntax.load('slovnet_syntax_news_v1.tar')
     syntax_parser.navec(navec)
 
-    st = time.time()
-    for i in range(50):
-        sint = get_sintax_list2(syntax_parser, book_xml)
-    print(time.time() - st)
-    for i in sint:
-        words, deps = i
-        if words and deps:
-            show_markup(words, deps)
-        print()
-        print()
+    for book_num in range(70162, 160000):
+        book_path = config.raw_book_path + r'\flibusta_' + str(book_num) + '.zip'
+        optimized_sintax_path = config.optimized_sintax_path + r'\opt_sinx_' + str(book_num) + '.zip'
+        try:
+            book_xml = book_manager.open_book(book_path)
+            # book_info = get_base_info_from_book_xml(book_xml)
+            print(book_num)
+
+            sintax_list = get_sintax_list_with_navec(syntax_parser, book_xml)
+
+            with open(optimized_sintax_path, 'wb') as f:
+                clf = pickle.dump(sintax_list, f)
+                print("Сохранено :) ")
+        except zipfile.BadZipFile:
+            # book = open(book_path, 'r', encoding='utf8')
+            continue  # Там есть полезная инфа, но книги нет
+        except IndexError as e:
+            print("Ошибка в поиске")
+            print(e.args)
+        except etree.XMLSyntaxError:
+            print("Ошибка в парсинге")
