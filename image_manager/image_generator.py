1bb8b53bf099adac381e5d28d1e3658d0cca6c22
SlavaRom <ne555220@yandex.ru>
2021-01-07 22:39:13 +0300

    Разбиение от 1 до 3 строк. Три оценки(разбиения, позиционирования, отношение высоты к длине)

--- image_manager/image_generator.py
+++ image_manager/image_generator.py
@@ -6,7 +6,8 @@ from text_mining.sintaxis import SintaxisNavec
 import re
 
 DIFFERENCE_LENGTHS = 60
-
+W = 512
+H = 256
 
 def format_text(text, font, x_win, y_win):
     font_size = 50
@@ -86,54 +87,119 @@ def get_best_del(splited_text, part, size):
             return best_del
     return best_del
 
+def remove_all(text, sep):
+    try:
+        while True:
+            text.remove(sep)
+    except ValueError:
+        pass
 
 def split_text(text, sintaxis, count_lines, q_all_nsubj, count_signs):
-    splited_text = text.split()
     if count_lines == 1:
-        return [text], 1
+        return [[text]], 1
+    splited_text = re.split("(\W)", text)
+    remove_all(splited_text, '')
+    remove_all(splited_text, ' ')
+    max_rating = -1
+    best_split = []
     if count_lines == 2:
-        max_rating = -1
-        best_split = None
         for i in range(1, len(splited_text) - 1):
             current_split = [splited_text[:i], splited_text[i:]]
-            # for j in range(0, len(current_split)):
-            #     txt = ""
-            #     for h in current_split[j]:
-            #         txt = txt + ' ' + h
-            #     current_split[j] = txt
-            current_rating = splitting_rating(current_split, sintaxis, q_all_nsubj, count_signs)
+            current_rating = splitting_rating(current_split, q_all_nsubj, count_signs)
             print(current_rating)
             if current_rating >= max_rating:
+                if current_rating > max_rating:
+                    best_split.clear()
                 max_rating = current_rating
-                best_split = current_split
-        for j in range(0, len(best_split)):
-            txt = ""
-            for h in best_split[j]:
-                txt = txt + ' ' + h
-            best_split[j] = txt
-        best_split[0] = best_split[0][1:]
+                split = []
+                for current in current_split:
+                    string = ""
+                    for x in current:
+                        if x in "[{}.,-<>\";:!?[\]\\\/]":
+                            string = string + x
+                        else:
+                            string = string + " " + x
+                    split.append(string)
+                best_split.append(split)
+        return best_split, max_rating
+    if count_lines == 3:
+        for i in range(1, len(splited_text) - 2):
+            for j in range(i+1, len(splited_text)):
+                current_split = [splited_text[:i], splited_text[i:j], splited_text[j:]]
+                current_rating = splitting_rating(current_split, q_all_nsubj, count_signs)
+                if current_rating >= max_rating:
+                    if current_rating > max_rating:
+                        best_split.clear()
+                    max_rating = current_rating
+                    split = []
+                    for current in current_split:
+                        string = ""
+                        for x in current:
+                            if x in "[{}.,-<>\";:!?[\]\\\/]":
+                                string = string + x
+                            else:
+                                string = string + " " + x
+                        split.append(string)
+                    best_split.append(split)
         return best_split, max_rating
 
-
-def splitting_rating(split, sintaxis, q_all_nsubj, count_signs):
+def splitting_rating(split, q_all_nsubj, count_signs):
     count_signs_at_end = 0
-    for i in range(0, len(split) - 1):
-        # if split[i][-1] in "{}.,-<>\";:\!?[\]\\\/":
+    for i in range(0, len(split)):
         if split[i][-1][-1] in "{}.,-<>\";:\!?[\]\\\/":
             count_signs_at_end = count_signs_at_end + 1
-        q_inside_nsubj = 0
-        for j in q_all_nsubj:
-            if (j[0] >= len(split[0]) - 1 and j[1] >= len(split[0]) - 1) or (
-                    j[0] < len(split[0]) - 1 and j[1] < len(split[0]) - 1):
-                q_inside_nsubj = q_inside_nsubj + 1
-        q_inside_nsubj = len(q_all_nsubj) - q_inside_nsubj
+    q_inside_nsubj = 0
+    len_split_1 = len(split[0])
+    len_split_2 = len(split[1]) + len_split_1
+    for j in q_all_nsubj:
+        if (j[0] < len_split_1 and j[1] < len_split_1) or (
+                j[0] >= len_split_1 and j[0] < len_split_2 and j[1] >= len_split_1 and j[1] < len_split_2) or (
+                j[0] >= len_split_2 and j[1] >= len_split_2):
+            q_inside_nsubj = q_inside_nsubj + 1
     return (q_inside_nsubj + count_signs_at_end) / (len(q_all_nsubj) + count_signs)
 
+def best_phrase(splitted_text):
+    delete = []
+    for text in splitted_text:
+        #for j in range(0, len(text)):
+        for j in range(0, len(text) - 1):
+            if len(text[j]) < len(text[j+1]) or len(text[j]) == 1 or len(text[j + 1]) == 1:
+        #    if len(text[j]) == 1:
+                delete.append(text)
+                break
+    if len(delete) == len(splitted_text):
+        raise BaseException("Нет нормального разбиения")
+    for i in delete:
+        splitted_text.remove(i)
+    return splitted_text
+
+def position_rating(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font):
+    location = add_text_to_photo(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font)
+    window_w = img.shape[1] - margin[0] - margin[2]
+    summ = 0.0
+    max_w = -1
+    max_h = -1
+    for i in range(0, len(drop_text)):
+        cur_w = location[i][2] - location[i][0]
+        cur_h = location[i][1] - location[i][3]
+        if cur_w > max_w:
+            max_w = cur_w
+        if cur_h > max_h:
+            max_h = cur_h
+        real_w = cur_w/window_w
+        good_w = 1.0*alignment_horizontal_type[i]
+        summ = summ + (1.0 / abs(good_w - real_w))
+        qsh = max_h/max_w
+    return summ, qsh
 
 def put_text_pil(params):
     sint_maker = SintaxisNavec()
     text = params["text"]
     count_lines = params["count_lines"]
+    img = params["img"]
+    margin = params["margin"]
+    alignment_vertical_type = params["alignment_vertical_type"]
+    alignment_horizontal_type = params["alignment_horizontal_type"]
     sintaxis = sint_maker.get_sintaxis(text)
     count_signs = len(re.findall("[{}.,-<>\";:!?[\]\\\/]", text))
     q_all_nsubj = []
@@ -141,12 +207,18 @@ def put_text_pil(params):
         if i[2] == "nsubj":
             q_all_nsubj.append(i)
     splitted_text = split_text(text, sintaxis, count_lines, q_all_nsubj, count_signs)
-    img = np.zeros((256, 512, 3), np.uint8)  # или cv2.imread("path_to_file")
-    img[:, :, :] = 0
-    im = Image.fromarray(img)
-    drop_text = splitted_text[0]
+    print("Все разбивки")
+    print(splitted_text)
+    spl_rating = splitted_text[1]
+    try:
+        drop_text = best_phrase(splitted_text[0])[0]
+        print("Лучшие разбивки")
+        print(drop_text)
+    except BaseException as err:
+        print(err)
     test_text = "\n".join(drop_text)
-    print("Разбивка:\n" + test_text)
+    print("Лучшая разбивка:\n" + test_text)
+    im = Image.fromarray(img)
     font_size = find_font_size(test_text, '19925.ttf', im.width - 40, im.height)
     font = ImageFont.truetype('19925.ttf', size=font_size)
     # здесь узнаем размеры сгенерированного блока текста
@@ -166,7 +238,13 @@ def put_text_pil(params):
     dif = cur_min * 100.0 / cur_max
     print(dif)
     # Вывод текста с различными отступами
-    add_text_to_photo(draw, "uniform", (0, 0.5, 1, 0), (20, 20, 20, 20), drop_text, font_size, font)
+    pos_rating, qsh = position_rating(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font)
+    pos_rating = pos_rating * count_lines
+    spl_rating = spl_rating * count_lines
+    print("Рейтинг позиционирования:", str(pos_rating))
+    print("Рейтинг разбиения:", str(spl_rating))
+    print("Отношение максимальной высоты к максимальной длине:", str(qsh))
+    #add_text_to_photo(draw, "uniform", (1, 1, 1, 0), margin, drop_text, font_size, font)
     im.save("test_img.png", "png")
 
     img = np.asarray(im)
@@ -190,9 +268,10 @@ for i in wolf_idea:
     # print(split_text(i))
     pass
 
-img = np.zeros((256, 512, 3), np.uint8)  # или cv2.imread("path_to_file")
+img = np.zeros((H, W, 3), np.uint8)  # или cv2.imread("path_to_file")
 img[:, :, :] = 0
-params = {"text": "Будь осторожен. Одна ошибка - и ты ошибся.", "count_lines": 2}
+params = {"text": "Легкой бывает только легкая дорога. Тяжелая дорога всегда трудна.", "count_lines": 1,
+          "img": img, "margin": (20, 20, 20, 20), "alignment_vertical_type": "uniform", "alignment_horizontal_type": (1, 1, 1, 0)}
 img = put_text_pil(params)
 cv2.imshow('Result', img)
 cv2.waitKey()
