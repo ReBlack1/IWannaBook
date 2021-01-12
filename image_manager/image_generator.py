242ef1b0010f71b0179c7e787c722272d9569a94
SlavaRom <ne555220@yandex.ru>
2021-01-12 21:17:49 +0300

    Готово

--- image_manager/image_generator.py
+++ image_manager/image_generator.py
@@ -2,7 +2,7 @@ import PIL
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 import cv2
-from text_mining.sintaxis import SintaxisNavec
+from IWannaBook.text_mining.sintaxis import SintaxisNavec
 import re
 from guppy import hpy
 import time
@@ -10,6 +10,7 @@ import time
 DIFFERENCE_LENGTHS = 60
 W = 512
 H = 256
+STANDARD_FONT_SIZE = 16
 
 
 def find_font_size(test_text, font_path, x_win, y_win):
@@ -34,14 +35,15 @@ def get_nsubj_list(sintaxis):
             q_all_nsubj.append(syntactic_links)
     return q_all_nsubj
 
+
 def find_dif(cur_text):
     cur_min = 200
     cur_max = 0
     for syntactic_links in cur_text:
         if (len(syntactic_links) < cur_min):
-            cur_min = len(i)
+            cur_min = len(syntactic_links)
         if (len(syntactic_links) > cur_max):
-            cur_max = len(i)
+            cur_max = len(syntactic_links)
     dif = cur_min / cur_max * 100
     return dif
 
@@ -52,7 +54,7 @@ def find_alignment_vertical_type(dif, count_lines):
     return "centre"
 
 
-def add_text_to_photo(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font):
+def add_text_to_photo(img, draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font):
     """
     :param draw: обьект, на котором рисуем
     :param alignment_vertical_type: тип вертикального выравнивания
@@ -64,11 +66,16 @@ def add_text_to_photo(draw, alignment_vertical_type, alignment_horizontal_type, 
     :return:
     """
     location = []
-    w, h = font.getsize_multiline(drop_text[0])
+    try:
+        w, h = font.getsize_multiline(drop_text[0])
+    except IndexError:
+        return None
     if alignment_vertical_type == "uniform":  # вертикальное заполнение - равномерно распределенный текст
-        vertical_indent = (img.shape[0] - h * len(drop_text) - margin[1] - margin[3]) / len(drop_text) + 1  # Рассчитываем отступ по вертикали
+        vertical_indent = (img.shape[0] - h * len(drop_text) - margin[1] - margin[3]) / len(
+            drop_text) + 1  # Рассчитываем отступ по вертикали
     else:  # вертикальное заполнение - центированный текст
-        vertical_indent = (img.shape[0] - h * (2 * len(drop_text) - 1) - margin[1] - margin[3]) / 2  # Рассчитываем отступ по вертикали
+        vertical_indent = (img.shape[0] - h * (2 * len(drop_text) - 1) - margin[1] - margin[
+            3]) / 2  # Рассчитываем отступ по вертикали
     current_vert_indent = vertical_indent  # текущий вертикальный отступ
     for i in range(len(drop_text)):
         current_w, current_h = font.getsize_multiline(drop_text[i])  # Получаем размеры текущей строки
@@ -154,7 +161,11 @@ def splitting_rating(split, q_all_nsubj, count_signs):
                 j[0] >= len_split_1 and j[0] < len_split_2 and j[1] >= len_split_1 and j[1] < len_split_2) or (
                 j[0] >= len_split_2 and j[1] >= len_split_2):
             q_inside_nsubj = q_inside_nsubj + 1
-    return (q_inside_nsubj + count_signs_at_end) / (len(q_all_nsubj) + count_signs)
+    try:
+        rating = (q_inside_nsubj + count_signs_at_end) / (len(q_all_nsubj) + count_signs)
+    except ZeroDivisionError:
+        rating = 0
+    return rating
 
 
 def exclude_bad_phrases(splitted_text):
@@ -187,13 +198,13 @@ def merge_str(drop_text):
     return drop_text
 
 
-def position_rating(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font):
-    location = add_text_to_photo(draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size,
+def position_rating(img, draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size, font):
+    location = add_text_to_photo(img, draw, alignment_vertical_type, alignment_horizontal_type, margin, drop_text, font_size,
                                  font)
     for i in location:
         if i[0] < margin[0] or i[0] > W - margin[2] or i[1] < margin[1] or i[1] > H - margin[3] or i[2] > W - margin[
             2] or i[2] < margin[0] or i[3] < margin[1] or i[3] > H - margin[3]:
-            raise BaseException("Текст не помещается на картинку")
+            return None, None
     window_w = img.shape[1] - margin[0] - margin[2]
     summ = 0.0
     max_w = -1
@@ -241,13 +252,12 @@ def put_text_pil(params):
 
     best_pos_rating = -1
     best_split = ""
+    img = np.zeros((H, W, 3), np.uint8)
+    img[:, :, :] = 0
+    im = Image.fromarray(img)
+    draw = ImageDraw.Draw(im)
     for cur_text in drop_text:
         test_text = "\n".join(cur_text)
-        #im, draw, font, font_size = func(test_text, margin)
-        img = np.zeros((H, W, 3), np.uint8)
-        img[:, :, :] = 0
-        im = Image.fromarray(img)
-        draw = ImageDraw.Draw(im)
         font = params["font"]
         font_size = find_font_size(test_text, font, im.width - margin[0] - margin[2],
                                    im.height - margin[1] - margin[3])
@@ -256,19 +266,20 @@ def put_text_pil(params):
         dif = find_dif(cur_text)
         # Выбор типа вертикального выравнивания
         alignment_vertical_type = find_alignment_vertical_type(dif, count_lines)
-        try:
-            pos_rating, qsh = position_rating(draw, alignment_vertical_type, alignment_horizontal_type, margin,
-                                              cur_text,
-                                              font_size, font)
-        except BaseException as err:
-            raise BaseException(err)
-
-        im.save(f"test_img{count_lines, font_size}.png", "png")  # Сохраняет все созданные картинки
+        pos_rating, qsh = position_rating(img, draw, alignment_vertical_type, alignment_horizontal_type, margin,
+                                          cur_text,
+                                          font_size, font)
+        if pos_rating is None or qsh is None:
+            print("Текст не помещается на картинку!")
+            continue
 
         pos_rating = pos_rating * count_lines
         if pos_rating > best_pos_rating:
             best_pos_rating = pos_rating
             best_split = cur_text
+
+    if not isinstance(best_split, list) and len(best_split) < 2:
+        return None, 10000000000
 
     print("-----------------------")
     print("Лучшее разбиение: ", best_split)
@@ -277,15 +288,46 @@ def put_text_pil(params):
     print("Отношение максимальной высоты к максимальной длине:", str(qsh))
     print("-----------------------\n\n")
 
-
-    im = Image.open(img_path)
+    if img_path is None:
+        img = np.zeros((H, W, 3), np.uint8)
+        img[:, :, :] = 0
+        im = Image.fromarray(img)
+    else:
+        im = Image.open(img_path)
     draw = ImageDraw.Draw(im)
-    add_text_to_photo(draw, alignment_vertical_type, alignment_horizontal_type, margin, best_split, font_size, font)
+    add_text_to_photo(img, draw, alignment_vertical_type, alignment_horizontal_type, margin, best_split, font_size, font)
     im.save(f"final_img{count_lines}.png", "png")  # Сохраняет лучшую картинку
 
-    img = np.asarray(im)
-    return img
+    # img = np.asarray(im)
+    return im, font_size
 
+
+def find_best_photo(img_list, font_size_list):
+    dif = 200
+    for i in range(0, len(img_list)):
+        if abs(font_size_list[i] - STANDARD_FONT_SIZE) < dif:
+            best_img = img_list[i]
+            dif = abs(font_size_list[i] - STANDARD_FONT_SIZE)
+    return best_img
+
+
+def text_on_picture(text, img_path=None, font="14539.ttf", margin=(20, 20, 20, 20),
+                    alignment_horizontal_type=(1, 1, 1, 1)):
+    img_list = []
+    font_size_list = []
+    # img = np.zeros((H, W, 3), np.uint8)
+    # img_path = "fon.jpg"
+    params = {"text": text, "font": font, "img_path": img_path,
+              "margin": margin, "alignment_horizontal_type": alignment_horizontal_type}
+    for count_line in range(1, 5):
+        print("Длина разбиения: ", str(count_line))
+        params['count_lines'] = count_line
+        cur_img, font_size = put_text_pil(params)
+        img_list.append(cur_img)
+        font_size_list.append(font_size)
+    best_img = find_best_photo(img_list, font_size_list)
+    #best_img.save("BEST.png", "png")  # Сохраняет лучшую картинку
+    return best_img
 
 wolf_idea = [
     "Кем бы ты ни был, кем бы ты не стал, помни, где ты был и кем ты стал.",
@@ -299,27 +341,16 @@ wolf_idea = [
     "Будь осторожен. Одна ошибка - и ты ошибся.",
     "Привет, мир!"
 ]
-for i in wolf_idea:
-    # print(split_text(i))
-    pass
 
-# start = time.time()
-img = np.zeros((H, W, 3), np.uint8)  # или cv2.imread("path_to_file")
-img_path = "fon.jpg"
-font = "14539.ttf"
-params = {"text": "Кем бы ты ни был, кем бы ты не стал, помни, где ты был и кем ты стал.",
-          "img_path": img_path, "font": font, "margin": (20, 20, 20, 20), "alignment_horizontal_type": (1, 1, 1, 1)}
-for count_line in range(1, 5):
-    try:
-        print("Длина разбиения: ", str(count_line))
-        params['count_lines'] = count_line
-        img = put_text_pil(params)
-    except BaseException as err:
-        print(err)
-        continue
-cv2.imshow('Result', img)
-h = hpy()
-# end = time.time()
-# print(end - start)
-# print(h.heap())
-cv2.waitKey()
+if __name__ == '__main__':
+    # start = time.time()
+    text = "Даже если нет шансов, всегда есть шанс."
+    best_img = text_on_picture(text)
+    best_img.save("BEST.png", "png")
+    # best_img = np.asarray(best_img)
+    # cv2.imshow('Result', best_img)
+    # h = hpy()
+    # end = time.time()
+    # print(end - start)
+    # print(h.heap())
+    # cv2.waitKey()
