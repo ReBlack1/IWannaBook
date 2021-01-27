9d910e15a2f0886517baef5cefbbb0a9a9bd2d9a
SlavaRom <ne555220@yandex.ru>
2021-01-27 12:43:28 +0300

    Разбиение на функции

--- text_analys/classifiers/using.py
+++ text_analys/classifiers/using.py
@@ -1,5 +1,6 @@
 from text_analys.classifiers.classifier import *
 import pickle
+import time
 import networkx as nx
 import book_manager
 from lxml import etree
@@ -12,45 +13,47 @@ def open_graph_from_plc(path):
     return graph
 
 
+def find_abstract_list(graph, vectors, book_w_l, emotional_classifier):
+    vec_size = len(vectors)  # количество уникальных слов
+    abstract_words = []
+    emotional_answer = emotional_classifier.predict_by_vectors(vectors)
+    for i in range(vec_size):  # проходим по всем уникальным словам
+        word = book_w_l[i]  # текущее слово
+        if word.split('_')[-1] in ['NOUN', 'VERB', 'ADJ', 'PRTF']:  # если слова важная часть речи
+            if emotional_answer[i] == "abstract":
+                cnt_word = graph.nodes[word]["size"]  # количество слов, синонимичных текущему
+                abstract_words.append((word, cnt_word))
+    return abstract_words
+
+
+def find_cnt_all_words(graph, vectors, book_w_l):
+    vec_size = len(vectors)  # количество уникальных слов
+    cnt_all = 0
+    for i in range(vec_size):  # проходим по всем уникальным словам
+        word = book_w_l[i]  # текущее слово
+        cnt_all = cnt_all + graph.nodes[word]["size"]  # количество слов, синонимичных текущему
+    return cnt_all
+
 if __name__ == '__main__':
-    path = r"D:\Python\IWannaBook\text_analys\classifiers\opt_graph_10001.plc"
+    emotional_classifier = EmotionalClassifier()
+
+    start = time.time()
+
+    path = r"D:\Python\IWannaBook\text_analys\classifiers\opt_graph_10019.plc"
     opt_graph = open_graph_from_plc(path)
     book_w_l = opt_graph['book_word_list']
     vectors = opt_graph['vectors']
     graph = opt_graph['graph']
 
-    emotional_classifier = EmotionalClassifier()
-    cruel_classifier = CruelClassifier()
-    sexual_classifier = SexualClassifier()
-
+    abstract_word_list = find_abstract_list(graph, vectors, book_w_l, emotional_classifier)
+    cnt_all = find_cnt_all_words(graph, vectors, book_w_l)
     cnt_abstract = 0  # количество абстрактных слов
-    cnt_all = 0  # количество всех слов
-    vec_size = len(vectors)  # количество уникальных слов
-    words_like = {}  # словарь со всеми словами, разбитыми на группы
-
-    emotional_answer = emotional_classifier.predict_by_vectors(vectors)
-    cruel_answer = cruel_classifier.predict_by_vectors(vectors)
-    sexual_answer = sexual_classifier.predict_by_vectors(vectors)
-
-    for i in range(vec_size):  # проходим по всем уникальным словам
-        word = book_w_l[i]  # текущее слово
-        cnt_word = graph.nodes[word]["size"]  # количество слов, синонимичных текущему
-        cnt_all = cnt_all + cnt_word
-        if word.split('_')[-1] in ['NOUN', 'VERB', 'ADJ', 'PRTF']:  # если слова важная часть речи
-            emotional_appraisal = emotional_answer[i]
-            cruel_appraisal = cruel_answer[i]
-            sexual_appraisal = sexual_answer[i]
-            if words_like.get(emotional_appraisal, None) is None:
-                words_like[emotional_appraisal] = []
-            if words_like.get(cruel_appraisal, None) is None:
-                words_like[cruel_appraisal] = []
-            if words_like.get(sexual_appraisal, None) is None:
-                words_like[sexual_appraisal] = []
-            words_like[emotional_appraisal].append((word, cnt_word))
-            words_like[cruel_appraisal].append((word, cnt_word))
-            words_like[sexual_appraisal].append((word, cnt_word))
-    for word in words_like["abstract"]:
+    for word in abstract_word_list:
         cnt_abstract = cnt_abstract + word[1]
+
+    end = time.time()
+
+    print(end - start)
     print('Абстрактных слов: ', cnt_abstract)
     print('Всего слов: ', cnt_all)
     print('Книга абстрактна на ', str('{:.2f}'.format(cnt_abstract / cnt_all)))
